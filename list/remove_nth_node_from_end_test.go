df989530193599987ab188688401f0e33f5bd22a
seafoodWang <xiang_wangs@163.com>
2023-03-31 16:05:42 +0800

    modify files

--- list/remove_nth_node_from_end_test.go
+++ list/remove_nth_node_from_end_test.go
@@ -8,17 +8,33 @@ import (
 //leetcode-19 https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
 
 func TestRemoveNthNodeFromEndOfList1st(t *testing.T) {
-
+	size := 10
 	head := createList(10)
-	//aimIndex := 5
+	lastIndex := 10
+	if size == lastIndex {
+		head = head.Next
+		return
+	}
 	node := head
 	iteratorTimes := 0
 	for node.Next != nil {
 		iteratorTimes++
+		node = node.Next
 	}
-	fmt.Printf("%d", iteratorTimes)
+	iteratorTimes -= lastIndex
+	node = head
+	for i := 0; i < iteratorTimes; i++ {
+		node = node.Next
+	}
+	node.Next = node.Next.Next
+	for head != nil {
+		fmt.Println(head.Val)
+		head = head.Next
+	}
 }
 
+// two ptr
+// TODO
 func TestRemoveNthNodeFromEndOfList2nd(t *testing.T) {
 
 }
