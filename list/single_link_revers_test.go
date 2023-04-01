3518a131c2eabe41370aa233cac4554740844ebb
seafoodWang <xiang_wangs@163.com>
2023-04-02 00:11:26 +0800

    reverse linked list

--- list/single_link_revers_test.go
+++ list/single_link_revers_test.go
@@ -1,17 +1,30 @@
 package list
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLinkedListReverse(t *testing.T) {
 
 	head := createList(10)
+	h := reverse(head)
+	PrintNodeList(h)
+}
 
-	node := head
-	var pre *Node
-	for head.Next != nil {
-		cur := node
-		node = node.Next
-		cur.Next = pre
-		pre = cur
+func reverse(head *Node) *Node {
+	if head == nil {
+		return nil
 	}
+	var node *Node
+	for head != nil {
+		//原链表的头节点给cur
+		cur := head
+		//原链表头指针后移
+		head = head.Next
+		//cur的Next给node
+		cur.Next = node
+		//新链表头向前移动
+		node = cur
+	}
+	return node
 }
